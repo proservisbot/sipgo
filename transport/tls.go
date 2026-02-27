f5b4e743875abf983de7fa7507e4d1bd02210d28
Diarmuid Wrenne <diarmuid.wrenne@servisbot.com>
2026-02-27 14:04:47 +0000

    Add TCP keepalive and TLS handshake improvements for AWS NLB compatibility

--- transport/tls.go
+++ transport/tls.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net"
+	"time"
 
 	sipgo "github.com/emiago/sipgo/sip"
 
@@ -42,7 +43,7 @@ func (t *TLSTransport) String() string {
 func (t *TLSTransport) CreateConnection(laddr Addr, host string, raddr Addr, handler sip.MessageHandler) (Connection, error) {
 	// raddr, err := net.ResolveTCPAddr("tcp", addr)
 	// if err != nil {
-	// 	return nil, err
+	//      return nil, err
 	// }
 
 	traddr := &net.TCPAddr{
@@ -50,7 +51,16 @@ func (t *TLSTransport) CreateConnection(laddr Addr, host string, raddr Addr, han
 		Port: raddr.Port,
 	}
 
-	return t.createConnection(nil, host, traddr, handler)
+	// PATCH: Pass local address if available
+	var tladdr *net.TCPAddr
+	if laddr.IP != nil {
+		tladdr = &net.TCPAddr{
+			IP:   laddr.IP,
+			Port: laddr.Port,
+		}
+	}
+
+	return t.createConnection(tladdr, host, traddr, handler)
 }
 
 func (t *TLSTransport) createConnection(laddr *net.TCPAddr, host string, raddr *net.TCPAddr, handler sip.MessageHandler) (Connection, error) {
@@ -66,14 +76,36 @@ func (t *TLSTransport) createConnection(laddr *net.TCPAddr, host string, raddr *
 		conf = &tls.Config{}
 	}
 	conf.ServerName = host
+
+	// PATCH: Add timeout and TCP keepalive to prevent AWS NLB flow tracking timeouts
 	dialer := &net.Dialer{
 		LocalAddr: laddr,
+		Timeout:   30 * time.Second,  // Connection timeout
+		KeepAlive: 30 * time.Second,  // TCP keepalive interval
 	}
-	conn, err := dialer.DialContext(context.TODO(), "tcp", addr)
+
+	// PATCH: Use context with timeout instead of context.TODO()
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	conn, err := dialer.DialContext(ctx, "tcp", addr)
 	if err != nil {
 		return nil, fmt.Errorf("%s dial err=%w", t, err)
 	}
+
+	// PATCH: Enable TCP keepalive on the connection
+	if tcpConn, ok := conn.(*net.TCPConn); ok {
+		_ = tcpConn.SetKeepAlive(true)
+		_ = tcpConn.SetKeepAlivePeriod(30 * time.Second)
+	}
+
 	tconn := tls.Client(conn, conf)
+
+	// PATCH: Perform TLS handshake with timeout
+	if err := tconn.HandshakeContext(ctx); err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("%s TLS handshake err=%w", t, err)
+	}
 
 	c := t.initConnection(tconn, addr, handler)
 	return c, nil
